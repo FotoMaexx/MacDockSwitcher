cdad579979124215c98c169e7e02c7c32a59a142
Maximilian Hauser <info@hauser-media.com>
2022-10-20 12:26:20 +0200

    Initial Commit

---
+++ MacDockSwitcher/MacDockSwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacDockSwitcherApp.swift
+//  MacDockSwitcher
+//
+//  Created by Maximilian Hauser on 20.10.22.
+//
+
+import SwiftUI
+
+@main
+struct MacDockSwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
