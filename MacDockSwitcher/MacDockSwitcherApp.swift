efe7cf8330c3b5f4502f1f92d6c473af6f67ec71
Bas O <basoppenheim@hotmail.com>
2023-03-11 22:45:51 +0100

    Updated UI, new "Apply Slow and Stable" option with loading menubar icon, new "Apply 'Normal' on Quit" option, Dock choice is remembered, Dock doesn't restart if Dock choice doesn't change.

--- MacDockSwitcher/MacDockSwitcherApp.swift
+++ MacDockSwitcher/MacDockSwitcherApp.swift
@@ -16,37 +16,75 @@ struct MacDockSwitcherApp: App {
         initialize()
     }
 
-    @State var currentNumber: Int = 1
+    @AppStorage("currentNumber") var currentNumber: Int = 1
+    @AppStorage("quitToRevert") var quitToRevert: Bool = false
+    @AppStorage("safetyWait") var safetyWait: Bool = true
+    
+    @State var waitingToChange = false
+    
+    @State var startDate = Date.now
+    @State var timeElapsed: Double = 0.0
+    
+    @State var timer = Timer.publish(every: 0.25, on: .main, in: .common).autoconnect()
 
     var body: some Scene {
-        MenuBarExtra(String(currentNumber), systemImage: "\(currentNumber).circle") {
-            Button("Normal") {
-                switchScene(from: currentNumber, to: 1)
-                currentNumber = 1
+        MenuBarExtra(isInserted: .constant(true)) {
+            ForEach([1 : "Normal", 2 : "Study", 3 : "Work", 4 : "Rest"].sorted(by: <),  id: \.key) { key, value in
+                Button {
+                    if currentNumber != key {
+                        setDock(key)
+                    }
+                } label: {
+                    HStack {
+                        Image(systemName: key == currentNumber ? "\(key).circle.fill" : "\(key).circle")
+                            .symbolRenderingMode(.hierarchical)
+                        Text(value)
+                    }
+//                    Label(value, image: key == currentNumber ? "checkmark" : "\(key).circle")
+                }
+                .keyboardShortcut(.init(.init("\(key)")))
+                .disabled(waitingToChange)
             }
-            .keyboardShortcut("1")
-
-
-
-            Button("Uni") {
-                switchScene(from: currentNumber, to: 2)
-                currentNumber = 2
-            }
-                    .keyboardShortcut("2")
-
-            Button("COV-IT") {
-                switchScene(from: currentNumber, to: 3)
-                currentNumber = 3
-            }
-                    .keyboardShortcut("3")
+            
+            Divider()
+            
+            Toggle("Apply Slow and Stable", isOn: $safetyWait)
+            Toggle("Apply 'Normal' on Quit", isOn: $quitToRevert)
+            
             Divider()
 
             Button("Quit") {
-                switchScene(from: currentNumber, to: 1)
-                NSApplication.shared.terminate(nil)
-
+                if quitToRevert && currentNumber != 1  {
+                    setDock(1) {result in
+                        NSApplication.shared.terminate(nil)
+                    }
+                } else {
+                    NSApplication.shared.terminate(nil)
+                }
             }
                     .keyboardShortcut("q")
+                    .disabled(waitingToChange)
+        } label: {
+            Image(systemName: waitingToChange ? "rays" : "\(currentNumber).circle", variableValue: timeElapsed/5.0)
+                .onReceive(timer) { firedDate in
+                    print("timer fired")
+                    timeElapsed = firedDate.timeIntervalSince(startDate)
+                }
+        }
+    }
+        
+    func setDock(_ dockNumber: Int, completion:  ((Bool) -> Void)? = nil)  {
+        
+        startDate = Date.now
+        timeElapsed = 0.0
+        waitingToChange = true
+        timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+        DispatchQueue.main.asyncAfter(deadline: .now() + (safetyWait ? 5.0 : 0.1)) {
+            switchScene(from: currentNumber, to: dockNumber)
+            currentNumber = dockNumber
+            waitingToChange = false
+            timer.upstream.connect().cancel()
+            completion?(true)
         }
     }
 }
